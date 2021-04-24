9515cbd4c213c8f6429584aaacb87e21e23ccd7e
harshR <42332761+tetrex@users.noreply.github.com>
2021-04-24 23:27:55 +0530

    final

---
+++ data.js
@@ -0,0 +1,25 @@
+{
+  [
+    {
+      title: "this is the title os the meeting",
+      time: "4:30pm",
+      link:
+        "https://brie.fi/ng/outlying-football-68?audio=1&video=1&fs=1&invite=0&prefs=1&share=0",
+      urlTitle: "meetingChapter1",
+    },
+    {
+      title: "this is the title of 2nd meeting",
+      time: "2:20pm",
+      link:
+        "https://brie.fi/ng/outlyingJHGASDAD?audio=1&video=1&fs=1&invite=0&prefs=1&share=0",
+      urlTitle: "meetingChapter2",
+    },
+    {
+      title: "this is the title of 3rd meeting",
+      time: "6:40pm",
+      link:
+        "https://brie.fi/ng/outlyingLKIPOJJIOJKL?audio=1&video=1&fs=1&invite=0&prefs=1&share=0",
+      urlTitle: "meetingChapter4",
+    },
+  ];
+}
